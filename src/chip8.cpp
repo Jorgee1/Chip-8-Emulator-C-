a6edc8fdd86d80dd5ef8952436012a5dda9cfb0d
jorgee1 <jorgepacheco@uninorte.edu.co>
2020-03-23 13:44:55 -0500

    Arreglado scrolling en x, el calulo es un poco extraño pero como es un int se borran todos los decimales

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -95,34 +95,49 @@ void Chip8::cpu2_code(){
 }
 
 void Chip8::cpu3_code(){
-    if(V[(op&0x0F00)>>8] == (op&0x00FF) ){
+    int x  = (op&0x0F00)>>8;
+    int kk = op&0x00FF;
+    
+    if(V[x] == kk){
         pc+=2;
     }
     pc+=2;
 }
 
 void Chip8::cpu4_code(){
-    if(V[(op&0x0F00)>>8] != (op&0x00FF)){
+    int x  = (op&0x0F00)>>8;
+    int kk = op&0x00FF;
+    
+    if(V[x] != kk){
         pc+=2;
     }
     pc+=2;
 }
 
 void Chip8::cpu5_code(){
-    if(V[(op&0x0F00)>>8] == V[(op&0x00F0)>>4]){
+    int x = (op&0x0F00)>>8;
+    int y = (op&0x00F0)>>4;
+    
+    if(V[x] == V[y]){
         pc+=2;
     }
     pc+=2;
 }
 
 void Chip8::cpu6_code(){
-    V[(op&0x0F00)>>8] = (op&0x00FF);
+    int x  = (op&0x0F00)>>8;
+    int kk = op&0x00FF;
+    
+    V[x] = kk;
     pc+=2;
 }
 
 void Chip8::cpu7_code(){
-    V[(op&0x0F00)>>8] += (op&0x00FF);
-    V[(op&0x0F00)>>8] &= 0xFF;
+    int x  = (op&0x0F00)>>8;
+    int kk = op&0x00FF;
+
+    V[x] += kk;
+    V[x] &= 0xFF;
     pc+=2;
 }
 
@@ -130,74 +145,98 @@ void Chip8::cpu8_code(){
     switch(op&0x000F){
 
         case 0x0000:{
-            V[(op&0x0F00)>>8] = V[(op&0x00F0)>>4];
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            V[x] = V[y];
             break;
         }
 
         case 0x0001:{
-            V[(op&0x0F00)>>8] |= V[(op&0x00F0)>>4];
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            V[x] |= V[y];
             break;
         }
 
         case 0x0002:{
-            V[(op&0x0F00)>>8] &= V[(op&0x00F0)>>4];
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            V[x] &= V[y];
             break;
         }
 
         case 0x0003:{
-            V[(op&0x0F00)>>8] ^= V[(op&0x00F0)>>4];
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            V[x] ^= V[y];
             break;
         }
 
         case 0x0004:{
-            int x = V[(op&0x0F00)>>8]&0xFF;
-            int y = V[(op&0x00F0)>>4]&0xFF;
-            int z = 0;
-            if(x+y > 0xFF){
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            
+            int vx = V[x];
+            int vy = V[y];
+            
+            if(vx + vy > 0xFF){
                 V[0xF] = 0x01;
             }else{
                 V[0xF] = 0x00;
             }
-            z = (x + y)&0xFF;
-            V[(op&0x0F00)>>8] = z;
+            
+            V[x] += vy;
+            V[x] &= 0xFF;
             break;
         }
 
         case 0x0005:{
-            int x = V[(op&0x0F00)>>8]&0xFF;
-            int y = V[(op&0x00F0)>>4]&0xFF;
-            if(x > y){
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            
+            int vx = V[x];
+            int vy = V[y];
+            
+            if(vx > vy){
                 V[0xF] = 0x01;
             }else{
                 V[0xF] = 0x00;
             }
-            V[(op&0x0F00)>>8] -= V[(op&0x00F0)>>4];
-            V[(op&0x0F00)>>8] &= 0xFF;
+            
+            V[x] -= vy;
+            V[x] &= 0xFF;
             break;
         }
 
         case 0x0006:{
-            V[0xF] = V[(op&0x0F00)>>8]&0x01;
-            V[(op&0x0F00)>>8] = V[(op&0x0F00)>>8] >> 1;
+            int x = (op&0x0F00)>>8;
+            V[0xF] = V[x]&1;
+            V[x] >>= 1;
             break;
         }
 
         case 0x0007:{
-            int x = V[(op&0x00F0)>>4]&0xFF;
-            int y = V[(op&0x0F00)>>8]&0xFF;
-            if(y > x){
+            int x = (op&0x0F00)>>8;
+            int y = (op&0x00F0)>>4;
+            
+            int vx = V[x];
+            int vy = V[y];
+            
+            if(vy > vx){
                 V[0xF] = 0x01;
             }else{
                 V[0xF] = 0x00;
             }
-            V[(op&0x0F00)>>8] -= V[(op&0x0F00)>>4];
-            V[(op&0x0F00)>>8] &= 0xFF;
+            V[x] -= vy;
+            V[x] &= 0xFF;
             break;
         }
 
         case 0x000E:{
-            V[0xF] = (V[(op&0x0F00)>>8]&0x80)>>7;
-            V[(op&0x0F00)>>8] = (V[(op&0x0F00)>>8] << 1)&0xFF;
+            int x = (op&0x0F00)>>8;
+            V[0xF] = (V[x]&0x80)>>7;
+            V[x] <<= 1;
+            V[x] &= 0xFF;
             break;
         }
 
@@ -206,9 +245,13 @@ void Chip8::cpu8_code(){
 }
 
 void Chip8::cpu9_code(){
-    if(V[(op&0x0F00)>>8] != V[(op&0x00F0)>>4]){
+    int x = (op&0x0F00)>>8;
+    int y = (op&0x00F0)>>4;
+    
+    if(V[x] != V[y]){
         pc+=2;
     }
+    
     pc+=2;
 }
 
@@ -218,12 +261,16 @@ void Chip8::cpuA_code(){
 }
 
 void Chip8::cpuB_code(){
-    pc = V[0] + (op&0x0FFF);
+    int nnn = op&0x0FFF;
+    pc = (nnn + V[0])&0xFFFF;
 }
 
 void Chip8::cpuC_code(){
-    int x = rand () % 255;
-    V[(op & 0x0F00) >> 8] = x & (op & 0x00FF);
+    int x  = (op & 0x0F00) >> 8;
+    int kk = op & 0x00FF;
+    int rnd = rand() % 256;
+    
+    V[x] = rnd & kk;
     pc+=2;
 }
 
@@ -235,7 +282,12 @@ void Chip8::cpuD_code(){
     V[0xF] = 0;
     
     unsigned long temp_sprite = 0;
-    unsigned long temp_screen = 0;                   
+    unsigned long temp_screen = 0;
+
+    if (x>=w){
+        x -= (x/w)*w;
+    }
+                      
     int fase = w-8-x;
 
     
@@ -361,7 +413,7 @@ void Chip8::cycle(){
         dt--;
     }
     if(st > 0){
-        printf("Beep\n");
+        //printf("Beep\n");
         st--;
     }
 }
