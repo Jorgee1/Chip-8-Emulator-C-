46f25db2972201e0c4645eb16cc36777eb4959c1
jorgee1 <jorgepacheco@uninorte.edu.co>
2020-03-23 15:04:09 -0500

    Implementación de bandera para activar el modo debug

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,18 @@ std::string to_format_str(char format[], int value){
 }
 
 int main( int argc, char* args[] ){
-    int SCREEN_WIDTH  = 640;
-    int SCREEN_HEIGHT = 600;
-    int TEXT_SIZE     =  15;
-    int upscale       =   5;
-    int scrollIndex   = 0x2E7;    
-    bool exit = false;   
     std::string NAME = "Chip 8";
+    
+    int TEXT_SIZE   =  15;
+    int upscale     =  10;
+    int scrollIndex = 0x2E7; 
+       
+    bool exit  = false;
+    bool debug = false;
+    
+    if (debug){
+        upscale = 4;
+    }
     
     /*
         CHIP 8 INIT
@@ -34,7 +39,14 @@ int main( int argc, char* args[] ){
     
     int AnchoP = chip8.w*upscale;
     int LargoP = chip8.h*upscale;
+    int SCREEN_WIDTH  = AnchoP;
+    int SCREEN_HEIGHT = LargoP;
     
+    if (debug){
+        SCREEN_WIDTH  = 600;
+        SCREEN_HEIGHT = 600;
+    }
+
     /*
         LOAD MEMORY
     */
@@ -128,114 +140,123 @@ int main( int argc, char* args[] ){
                 Render Grafics
             */
             
-            int layout_y = 0;
-            text_white.render(
-                AnchoP, layout_y,
-                "PC:" + to_format_str("0x%04X", chip8.pc) +
-                " " +
-                std::to_string(chip8.pc-0x200)
-            );
-            layout_y += TEXT_SIZE;
             
-            text_white.render(
-                AnchoP, layout_y,
-                "OPCODE:" + to_format_str("0x%04X", chip8.op)
-            );
-            layout_y += TEXT_SIZE;
-            
-            text_white.render(
-                AnchoP, layout_y,
-                "INDEX:" + to_format_str("0x%04X", chip8.I)
-            );
-            layout_y += TEXT_SIZE;
-
-            text_white.render(
-                AnchoP, layout_y,
-                "StackP:" + to_format_str("0x%04X", chip8.sp)
-            );
-            layout_y += TEXT_SIZE;
-
-            text_white.render(
-                AnchoP, layout_y,
-                "TIMER_D:" + to_format_str("0x%04X", chip8.dt)
-            );
-            layout_y += TEXT_SIZE;
-            
-            text_white.render(
-                AnchoP, layout_y,
-                "TIMER_S:" + to_format_str("0x%04X", chip8.st)
-            );
-            layout_y += TEXT_SIZE;
-            
-            for(int i=0; i<16; i++){
+            if(debug == true){
+                int layout_y = 0;
                 text_white.render(
                     AnchoP, layout_y,
-                    "V[" + to_format_str("%01X", i) + "]:" +
-                    to_format_str("0x%04X", chip8.V[i])
+                    "PC:" + to_format_str("0x%04X", chip8.pc) +
+                    " " +
+                    std::to_string(chip8.pc-0x200)
                 );
                 layout_y += TEXT_SIZE;
-            }
-            
-            layout_y += TEXT_SIZE;
-            for(int i=0; i<16; i++){
+                
                 text_white.render(
                     AnchoP, layout_y,
-                    "Key[" + to_format_str("%01X", i) + "]:" +
-                    to_format_str("0x%04X", chip8.key[i])
+                    "OPCODE:" + to_format_str("0x%04X", chip8.op)
                 );
                 layout_y += TEXT_SIZE;
+                
+                text_white.render(
+                    AnchoP, layout_y,
+                    "INDEX:" + to_format_str("0x%04X", chip8.I)
+                );
+                layout_y += TEXT_SIZE;
+
+                text_white.render(
+                    AnchoP, layout_y,
+                    "StackP:" + to_format_str("0x%04X", chip8.sp)
+                );
+                layout_y += TEXT_SIZE;
+
+                text_white.render(
+                    AnchoP, layout_y,
+                    "TIMER_D:" + to_format_str("0x%04X", chip8.dt)
+                );
+                layout_y += TEXT_SIZE;
+                
+                text_white.render(
+                    AnchoP, layout_y,
+                    "TIMER_S:" + to_format_str("0x%04X", chip8.st)
+                );
+                layout_y += TEXT_SIZE;
+                
+                for(int i=0; i<16; i++){
+                    text_white.render(
+                        AnchoP, layout_y,
+                        "V[" + to_format_str("%01X", i) + "]:" +
+                        to_format_str("0x%04X", chip8.V[i])
+                    );
+                    layout_y += TEXT_SIZE;
+                }
+                
+                layout_y += TEXT_SIZE;
+                for(int i=0; i<16; i++){
+                    text_white.render(
+                        AnchoP, layout_y,
+                        "Key[" + to_format_str("%01X", i) + "]:" +
+                        to_format_str("0x%04X", chip8.key[i])
+                    );
+                    layout_y += TEXT_SIZE;
+                }
+                
+                layout_y = 0;
+                for(int i=15; i>=0; i--){
+                    if (chip8.sp == i){
+                        text_white.render(
+                            AnchoP+150, layout_y,
+                            "*STACK[" + to_format_str("%01X", i) + "]:" +
+                            to_format_str("0x%04X", chip8.stack[i])
+                        );
+                    }else{
+                        text_white.render(
+                            AnchoP+150, layout_y,
+                            " STACK[" + to_format_str("%01X", i) + "]:" +
+                            to_format_str("0x%04X", chip8.stack[i])
+                        );
+                    }
+                    layout_y += TEXT_SIZE;
+                }
+                
+                /*        
+                    Memory Scroll
+                */
+
+                layout_y = 0;
+                for(int i=0; i<10; i++){
+                    int memory_value_temp = chip8.memory[i + scrollIndex];
+                    if(chip8.pc == i + scrollIndex){
+                        text_white.render(
+                            0, 350+layout_y,
+                            to_format_str("*0x%04X", i + scrollIndex) + " " +
+                            to_format_str("0x%02X", memory_value_temp) + " " +
+                            std::bitset <8>(memory_value_temp).to_string()
+                        );
+                    }else if (chip8.I == i + scrollIndex){
+                        text_white.render(
+                            0, 350+layout_y,
+                            to_format_str("I0x%04X", i + scrollIndex) + " " +
+                            to_format_str("0x%02X", memory_value_temp) + " " +
+                            std::bitset <8>(memory_value_temp).to_string()
+                        );
+                    }else{
+                        text_white.render(
+                            0, 350+layout_y,
+                            to_format_str(" 0x%04X", i + scrollIndex) + " " +
+                            to_format_str("0x%02X", memory_value_temp) + " " +
+                            std::bitset <8>(memory_value_temp).to_string()
+                        );
+                    }
+
+                    layout_y += TEXT_SIZE;
+                }
+                if((action->get_state(action->BUTTON_MOVE_UP))&&(scrollIndex>0)){
+                    scrollIndex--;
+                }else if((action->get_state(action->BUTTON_MOVE_DOWN))&&(scrollIndex<0xFFFF-10)){
+                    scrollIndex++;
+                }
             }
             
-            layout_y = 0;
-            for(int i=15; i>=0; i--){
-                if (chip8.sp == i){
-                    text_white.render(
-                        AnchoP+150, layout_y,
-                        "*STACK[" + to_format_str("%01X", i) + "]:" +
-                        to_format_str("0x%04X", chip8.stack[i])
-                    );
-                }else{
-                    text_white.render(
-                        AnchoP+150, layout_y,
-                        " STACK[" + to_format_str("%01X", i) + "]:" +
-                        to_format_str("0x%04X", chip8.stack[i])
-                    );
-                }
-                layout_y += TEXT_SIZE;
-            }
-            
-            /*        
-                Memory Scroll
-            */
-
-            layout_y = 0;
-            for(int i=0; i<10; i++){
-                int memory_value_temp = chip8.memory[i + scrollIndex];
-                if(chip8.pc == i + scrollIndex){
-                    text_white.render(
-                        0, 350+layout_y,
-                        to_format_str("*0x%04X", i + scrollIndex) + " " +
-                        to_format_str("0x%02X", memory_value_temp) + " " +
-                        std::bitset <8>(memory_value_temp).to_string()
-                    );
-                }else if (chip8.I == i + scrollIndex){
-                    text_white.render(
-                        0, 350+layout_y,
-                        to_format_str("I0x%04X", i + scrollIndex) + " " +
-                        to_format_str("0x%02X", memory_value_temp) + " " +
-                        std::bitset <8>(memory_value_temp).to_string()
-                    );
-                }else{
-                    text_white.render(
-                        0, 350+layout_y,
-                        to_format_str(" 0x%04X", i + scrollIndex) + " " +
-                        to_format_str("0x%02X", memory_value_temp) + " " +
-                        std::bitset <8>(memory_value_temp).to_string()
-                    );
-                }
-
-                layout_y += TEXT_SIZE;
-            }
             std::bitset <64> temp_screen;
             for(int y = 0;y < chip8.h;y++){
                 temp_screen = std::bitset <64>(chip8.screen[y]);
@@ -252,12 +273,6 @@ int main( int argc, char* args[] ){
                         );
                     }
                 }
-            }
-
-            if((action->get_state(action->BUTTON_MOVE_UP))&&(scrollIndex>0)){
-                scrollIndex--;
-            }else if((action->get_state(action->BUTTON_MOVE_DOWN))&&(scrollIndex<0xFFFF-10)){
-                scrollIndex++;
             }
             
             window.update_screen();
